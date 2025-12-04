8e82403fcfea0da7a28ad442832dfc70e79392f0
Charlie Baker <138892841+cmaximb@users.noreply.github.com>
2025-12-04 18:01:46 +0000

    printing grid p 2

--- Printing Department - 4/printing department/printing department/Program.cs
+++ Printing Department - 4/printing department/printing department/Program.cs
@@ -10,7 +10,7 @@ namespace printing_department
 {
     internal class Program
     {
-        static bool[,] find_adjacent_loo_roll(string[] file, int CenterY, int CenterX)
+        static bool[,] find_adjacent_paper_roll(string[] file, int CenterX, int CenterY)
         {
             bool[,] win = new bool[3, 3];
             int[] offset = { -1, 0, 1 };
@@ -22,10 +22,10 @@ namespace printing_department
                     int x_off = CenterX + offset[x];
                     int y_off = CenterY + offset[y];
 
-                    if (x_off >= 0 && y_off >= 0 && y_off < file.Length && x_off < file[0].Length)
-                        win[x, y] = file[y_off][x_off] == '@';
+                    if (x_off >= 0 && y_off >= 0 && y_off < file.Length && x_off < file[0].Length && (x != 1 || y != 1))
+                        win[x,y] = file[x_off][y_off] == '@';
                     else
-                        win[x, y] = false;
+                        win[x,y] = false;
                 }
             }
 
@@ -35,12 +35,12 @@ namespace printing_department
         static int sum_up_each_window(bool[,] window)
         {
             int sum = 0;
-
-            for (int x = 0; x <= 2; x++)
+            
+            for (int y = 0; y <= 2; y++)
             {
-                for (int y = 0; y <= 2; y++)
+                for (int x = 0; x <= 2; x++) 
                 {
-                    if (window[x, y])
+                    if (window[y,x])
                         sum += 1;
                 }
             }
@@ -51,37 +51,80 @@ namespace printing_department
         static void Main(string[] args)
         {
             string[] file = File.ReadAllLines("input.txt");
-            int total = 0;
+            int total = -1;
             bool[,] window = new bool[3,3];
+            int old_total = -1;
 
-            for (int l = 0; l < file.Length; l++)
+            bool Part2 = true;
+            StreamWriter writer = new StreamWriter("final_rolls_layout.txt");
+
+            int num_gone = 0;
+
+            while (total != 0)
             {
-                for (int c  = 0; c < file[l].Length; c++)
-                {
-                    int window_total = 5;
-                    if (file[l][c] == '@')
-                    { 
-                        window = find_adjacent_loo_roll(file, l, c);
+                old_total = total;
+                total = 0;
 
-                        window_total = sum_up_each_window(window);
-                    }
-                   
-                    if (window_total < 5)
+                //writer.Dispose();
+                //writer = new StreamWriter("final_rolls_layout.txt");
+                string[] newFile = (string[])file.Clone();
+
+                for (int l = 0; l < file.Length; l++)
+                {
+                    for (int c = 0; c < file[l].Length; c++)
                     {
-                        Console.Write("X");
-                        total++;
-                    }
-                    else
+                        int window_total = 4;
+
+                        if (file[l][c] == '@')
+                        {
+                            window = find_adjacent_paper_roll(file, l, c);
+
+                            window_total = sum_up_each_window(window);
+                        }
+
+                        if (window_total < 4)
+                        {
+                            newFile[l] = file[l].ReplaceAt(c, 'X');
+                            total++;
+                        }
+                        
+                        Console.Write($"{newFile[l][c]}");
+                    } 
+                    Console.WriteLine("");
+                }
+                
+                num_gone += total;
+
+                writer.WriteLine("");
+
+                int output = 0;
+                foreach (string line in file)
+                {
+                    foreach (char c in line)
                     {
-                        Console.Write($"{file[l][c]}");
+                        if (c == 'X')
+                            output++;
                     }
                 }
+                Console.WriteLine($" {output}"); 
+                file = newFile;
 
-                Console.WriteLine("");
+                if (!Part2)
+                    break;
             }
-            Console.WriteLine($"{total}");
-        }
 
-        
+            writer.Close();
+            Console.WriteLine($"{num_gone}");
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string ReplaceAt(this string input, int index, char newChar)
+        {
+            char[] chars = input.ToCharArray();
+            chars[index] = newChar;
+            return new string(chars);
+        }
     }
 }

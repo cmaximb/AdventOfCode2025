d5f71559c2dfe5ae35fb1e1ff928d0f10f3b0ae6
Charlie Baker <138892841+cmaximb@users.noreply.github.com>
2025-12-05 14:28:31 +0000

    day 5 part 1 kitchen

---
+++ Cafeteria - 5/Cafeteria - 5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafeteria___5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total;
+            string[] file = File.ReadAllLines("input.txt");
+            HashSet<double> invalidIngredients = new HashSet<double>();
+
+            var rangeslist = file.TakeWhile((l) => l != "").Select(x =>
+            {
+                var separated = x.Split('-');
+                return (double.Parse(separated[0]), double.Parse(separated[1]));
+            });
+
+            int i = Array.IndexOf(file, "");
+
+            string[] spoiled = new string[file.Length - rangeslist.Count() - 1];
+            Array.ConstrainedCopy(file, i + 1, spoiled, 0, spoiled.Length);
+
+            total = 0;
+
+            foreach (string s in spoiled)
+            {
+                bool fresh = false;
+                foreach (var r in rangeslist)
+                {
+                    if (double.Parse(s) >= r.Item1 && double.Parse(s) <= r.Item2)
+                    {
+                        fresh = true;
+                    }
+                }
+                if (fresh)
+                    total ++;
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}

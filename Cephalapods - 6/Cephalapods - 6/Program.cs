f37c1b837f3e837a1724afc0a70cef9ffc9b3a0c
Charlie Baker <138892841+cmaximb@users.noreply.github.com>
2025-12-06 15:20:34 +0000

    day 6 split part 1

---
+++ Cephalapods - 6/Cephalapods - 6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cephalapods___6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            string[] file = File.ReadAllLines("input.txt");
+            string[] separators = new string[] { " ", "  ", "   " };
+            double[] line_split = file[0].Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(x => double.Parse(x)).ToArray();
+            string[] operations = file[file.Length - 1].Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string line in file.Skip(1)) { 
+                if ("*+".Contains(line[0])) break;
+                int[] adders = line.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+                line_split = line_split.Select((x, c) => {
+                    if (operations[c] == "*")
+                        x *= adders[c];
+                    else if (operations[c] == "+")
+                        x += adders[c];
+                    return x;
+                } ).ToArray();
+            }
+
+            double total = 0;
+            foreach (var count in line_split)
+                total += count;
+
+            Console.WriteLine(total);
+        }
+    }
+}
